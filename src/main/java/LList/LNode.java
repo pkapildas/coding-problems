bdabe96f67b2a4cc042d45c8c83a427550c611a2
Kapil das <pkapildas@gmail.com>
2025-01-31 19:26:52 +0530

    merge linked list

--- src/main/java/LList/LNode.java
+++ src/main/java/LList/LNode.java
@@ -2,10 +2,26 @@ package LList;
 
 public class LNode {
     int data ;
-    LNode next;
+    public LNode next;
 
     public LNode(int data, LNode next) {
         this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public LNode getNext() {
+        return next;
+    }
+
+    public void setNext(LNode next) {
         this.next = next;
     }
 }
